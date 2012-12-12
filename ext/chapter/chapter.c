d2d7fc9198847cbb1af3c5b20b457a52dd5d8b07
youpy <youpy@buycheapviagraonlinenow.com>
2012-12-12 13:43:14 +0900

    fix for compilation errors and warnings

--- ext/chapter/chapter.c
+++ ext/chapter/chapter.c
@@ -6,22 +6,26 @@
 static VALUE
 rb_get_chapters(VALUE self, VALUE path) {
     char* fileName = StringValuePtr(path);
-    MP4FileHandle file = MP4Read(fileName, 0);
+    MP4FileHandle file = MP4Read(fileName);
+
+    MP4Chapter_t * chapters = 0;
+    uint32_t chapterCount = 0;
+
+    uint32_t i;
+    uint32_t pos = 0;
+    VALUE result = rb_ary_new();
+
+    MP4ChapterType chtp;
+
     if(file == MP4_INVALID_FILE_HANDLE )
     {
        rb_raise(rb_eException, "could not load file");
        return Qnil;
     }
 
-    MP4Chapter_t * chapters = 0;
-    uint32_t chapterCount = 0;
-
     // get the list of chapters
-    MP4ChapterType chtp = MP4GetChapters(file, &chapters, &chapterCount, MP4ChapterTypeAny);
+    chtp = MP4GetChapters(file, &chapters, &chapterCount, MP4ChapterTypeAny);
 
-    uint32_t i;
-    uint32_t pos = 0;
-    VALUE result = rb_ary_new();
     for (i = 0; i < chapterCount; ++i)
     {
       VALUE chapter = rb_hash_new();
@@ -30,27 +34,31 @@ rb_get_chapters(VALUE self, VALUE path) {
       rb_hash_aset(chapter, rb_str_new2("duration"), INT2NUM(chapters[i].duration));
       rb_hash_aset(chapter, rb_str_new2("position"), INT2NUM(pos));
       rb_ary_push(result, chapter);
-      pos = pos + chapters[i].duration;
+      pos = pos + (uint32_t)chapters[i].duration;
     }
     // free up the memory
     MP4Free(chapters);
-    MP4Close(file);
+    MP4Close(file, 0);
     return result;
 }
 
 static VALUE
 rb_set_chapters(VALUE self, VALUE path, VALUE chapters) {
     char* fileName = StringValuePtr(path);
-    MP4FileHandle file = MP4Modify(fileName, 0, 0);
+    MP4FileHandle file = MP4Modify(fileName, 0);
+
+    uint32_t chapter_len = (uint32_t)RARRAY_LEN(chapters);
+
+    uint32_t i;
+    MP4Chapter_t newChapters[chapter_len];
+    MP4Chapter_t *chapter = malloc(sizeof(MP4Chapter_t));
+
     if(file == MP4_INVALID_FILE_HANDLE )
     {
        rb_raise(rb_eException, "could not load file");
        return Qnil;
     }
-    uint32_t chapter_len = RARRAY_LEN(chapters);
-    uint32_t i;
-    MP4Chapter_t newChapters[chapter_len]; 
-    MP4Chapter_t *chapter = malloc(sizeof(MP4Chapter_t));
+
     for (i = 0; i < chapter_len; i++) {
 
         VALUE duration = rb_hash_aref(rb_ary_entry(chapters, i), rb_str_new2("duration"));
@@ -63,7 +71,7 @@ rb_set_chapters(VALUE self, VALUE path, VALUE chapters) {
     free(chapter);
 
     MP4SetChapters(file, &newChapters[0], chapter_len, MP4ChapterTypeQt);
-    MP4Close(file);
+    MP4Close(file, 0);
     return Qnil;
 }
 
